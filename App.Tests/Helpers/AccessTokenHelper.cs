82d39d87b22c3ddb154be8dd856964a5a9a78317
sharonrussell <sharon-russell@outlook.com>
2021-11-05 14:58:32 +0000

    Obfuscate the client secret

--- App.Tests/Helpers/AccessTokenHelper.cs
+++ App.Tests/Helpers/AccessTokenHelper.cs
@@ -9,10 +9,11 @@ public static class AccessTokenHelper
     public static async Task<string> GetAccessToken(IConfigurationSection auth0Settings)
     {
         var auth0Client = new AuthenticationApiClient(new Uri(auth0Settings["Domain"]));
+
         var tokenRequest = new ClientCredentialsTokenRequest()
         {
             ClientId = auth0Settings["ClientId"],
-            ClientSecret = auth0Settings["ClientSecret"],
+            ClientSecret = Environment.GetEnvironmentVariable("MyAPP_Auth0__ClientSecret"),
             Audience = auth0Settings["Audience"]
         };
         var tokenResponse = await auth0Client.GetTokenAsync(tokenRequest);
