2fa09c20bae32f6537fd111df74bd0c343037f2f
sharonrussell <sharon-russell@outlook.com>
2021-11-05 14:21:51 +0000

    Adding authentication to the integration tests and tidy up

---
+++ App.Tests/Helpers/AccessTokenHelper.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading.Tasks;
+using Auth0.AuthenticationApi;
+using Auth0.AuthenticationApi.Models;
+using Microsoft.Extensions.Configuration;
+
+public static class AccessTokenHelper
+{
+    public static async Task<string> GetAccessToken(IConfigurationSection auth0Settings)
+    {
+        var auth0Client = new AuthenticationApiClient(new Uri(auth0Settings["Domain"]));
+        var tokenRequest = new ClientCredentialsTokenRequest()
+        {
+            ClientId = auth0Settings["ClientId"],
+            ClientSecret = auth0Settings["ClientSecret"],
+            Audience = auth0Settings["Audience"]
+        };
+        var tokenResponse = await auth0Client.GetTokenAsync(tokenRequest);
+
+        return tokenResponse.AccessToken;
+    }
+}
--- App.Tests/IntegrationTests/HelloTests.cs
+++ App.Tests/IntegrationTests/HelloTests.cs
@@ -1,30 +1,68 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using App;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using Xunit;
 
-public class BasicTests
-    : IClassFixture<WebApplicationFactory<App.Startup>>
+public class HelloTests : IClassFixture<WebApplicationFactory<Startup>>
 {
-    private readonly WebApplicationFactory<App.Startup> _factory;
+    private readonly WebApplicationFactory<Startup> _factory;
+    private readonly IConfigurationSection auth0Settings;
 
-    public BasicTests(WebApplicationFactory<App.Startup> factory)
+    public HelloTests(WebApplicationFactory<Startup> factory)
     {
         _factory = factory;
+
+        auth0Settings = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build()
+            .GetSection("Auth0");
     }
 
     [Theory]
     [InlineData("/Hello?Name=Someone")]
-    public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+    public async Task Get_Endpoint_Unauthenticated_Return401(string url)
     {
-        // Arrange
         var client = _factory.CreateClient();
 
-        // Act
         var response = await client.GetAsync(url);
 
-        // Assert
-        response.EnsureSuccessStatusCode(); // Status Code 200-299
-        Assert.Equal("text/html; charset=utf-8",
-            response.Content.Headers.ContentType.ToString());
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
     }
+
+    [Theory]
+    [InlineData("/Hello?Name=Someone")]
+    public async Task Get_Endpoint_Unauthorized_Return401(string url)
+    {
+        var client = _factory.CreateClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", "This is an invalid token");
+
+        var response = await client.SendAsync(request);
+
+        Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+    }
+
+    [Theory]
+    [InlineData("/Hello?Name=Someone")]
+    public async Task Get_Endpoint_CorrectlyAuthorized_Returns200(string url)
+    {
+        var client = _factory.CreateClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+        var accessToken = await AccessTokenHelper.GetAccessToken(auth0Settings);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+        var response = await client.SendAsync(request);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
 }
